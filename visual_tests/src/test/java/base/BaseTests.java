552b36188082bd19731f6b46687f9e685830f9cb
jldeen <jedeen@outlook.com>
2020-03-06 17:04:10 -0800

    Update base tests for chromeoption arguements

--- visual_tests/src/test/java/base/BaseTests.java
+++ visual_tests/src/test/java/base/BaseTests.java
@@ -10,18 +10,28 @@ import org.junit.BeforeClass;
 import org.junit.AfterClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseTests {
 
   protected static WebDriver driver;
   protected static Eyes eyes;
   protected static HomePage homePage;
+  protected static ChromeOptions options = new ChromeOptions();
 
   @BeforeClass
   public static void setUp() {
-    WebDriverManager.chromedriver().setup();
-    driver = new ChromeDriver();
+    // WebDriverManager.chromedriver().setup();
+    // ChromeOptions options = new ChromeOptions();
+    // options.addArguments("--headless");
+    // driver = new ChromeDriver();
 
+    WebDriverManager.chromedriver().setup();
+    ChromeOptions ChromeOptions = new ChromeOptions();
+    ChromeOptions.addArguments("--headless", "--no-sandbox");
+    driver = new ChromeDriver(ChromeOptions);
+
+    // For use with Applitools
     eyes = new Eyes();
     homePage = new HomePage(driver);
   }
