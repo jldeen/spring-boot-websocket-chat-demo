e9591066fbb823c82538ea67a4a378a362c9e182
Jessica Deen <jessicadeen@me.com>
2020-03-10 17:40:37 -0700

    Applitools Branch Merge - RUNWHERE variable added
    RUNWHERE variable added, remove second branch

--- visual_tests/src/test/java/base/BaseTests.java
+++ visual_tests/src/test/java/base/BaseTests.java
@@ -12,25 +12,42 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+// container cicd packages
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+// begin test classes
 public class BaseTests {
 
   protected static WebDriver driver;
   protected static Eyes eyes;
   protected static HomePage homePage;
   protected static ChromeOptions options = new ChromeOptions();
+  private static String _runWhere;
 
   @BeforeClass
-  public static void setUp() {
-    // WebDriverManager.chromedriver().setup();
-    // ChromeOptions options = new ChromeOptions();
-    // options.addArguments("--headless");
-    // driver = new ChromeDriver();
-
-    WebDriverManager.chromedriver().setup();
+  public static void setUp() throws MalformedURLException, InterruptedException{
+  
+    _runWhere = System.getenv("RUNWHERE");
     ChromeOptions ChromeOptions = new ChromeOptions();
-    ChromeOptions.addArguments("--headless", "--no-sandbox");
-    driver = new ChromeDriver(ChromeOptions);
+    WebDriverManager.chromedriver().setup();
 
+    if (_runWhere.equals("local")) {
+      // Standard local visual test call
+      driver = new ChromeDriver();
+    } 
+    else if (_runWhere.equals("pipeline")) {
+      // build server headless chrome CI/CD example
+      ChromeOptions.addArguments("--headless", "--no-sandbox");
+      driver = new ChromeDriver(ChromeOptions);
+    }
+    else if (_runWhere.equals("container")) {
+      // selenium hub remote settings (container based CI/CD)
+      String Selenium = "http://selenium_hub:4444/wd/hub";
+      driver = new RemoteWebDriver(new URL(Selenium), ChromeOptions);
+    };
+      
     // For use with Applitools
     eyes = new Eyes();
     homePage = new HomePage(driver);
